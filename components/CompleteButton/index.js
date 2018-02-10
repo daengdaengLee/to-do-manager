6e87618791f294711f0681662bf19298a673275b
KunhoLee <gunho1020@gmail.com>
2018-02-10 23:06:31 +0900

    ENH: CompleteButton 컴포넌트에서 Button 컴포넌트와 CompleteButtonContent 컴포넌트 조합

--- components/CompleteButton/index.js
+++ components/CompleteButton/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Button from '../Button';
+import CompleteButtonContent from '../CompleteButtonContent';
+
 function CompleteButton() {
   return (
-    <Text>CompleteButton Component</Text>
+    <Button ownEvent={{ onPress: () => console.log('test')}} >
+      <CompleteButtonContent isCompleted={false} />
+    </Button>
   );
 }
 
