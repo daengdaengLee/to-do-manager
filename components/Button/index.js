0eb1f0ae35fa8abb42c92a56a5c8929bfcb9fa99
KunhoLee <gunho1020@gmail.com>
2018-02-10 22:03:34 +0900

    ENH: init Button Component

---
+++ components/Button/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+function Button() {
+  return (
+    <Text>
+      This is a Button Component.
+    </Text>
+  );
+}
+
+export default Button;
