69dd9bd92c17933a8381624dbe71625828d087d1
KunhoLee <gunho1020@gmail.com>
2018-02-10 23:48:09 +0900

    ENH: DeleteButtonContent 완성

--- components/DeleteButtonContent/index.js
+++ components/DeleteButtonContent/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 function DeleteButtonContent() {
   return (
-    <Text>DeleteButtonContent Component</Text>
+    <View style={styles.buttonContainer}>
+      <Text>
+        ❌
+      </Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default DeleteButtonContent;
