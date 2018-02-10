2380f47a97b4abacfbe3c0cc5d6a69e65a204163
KunhoLee <gunho1020@gmail.com>
2018-02-10 23:28:56 +0900

    ENH: init DeleteButtonContent Component

--- App.js
+++ App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import CompleteButton from './components/CompleteButton';
+import DeleteButtonContent from './components/DeleteButtonContent';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <CompleteButton
-          isCompleted={false}
-          ownEvent={{ onPress: () => console.log('test')}}
-        />
+        <DeleteButtonContent />
       </View>
     );
   }
---
+++ components/DeleteButtonContent/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+function DeleteButtonContent() {
+  return (
+    <Text>DeleteButtonContent Component</Text>
+  );
+}
+
+export default DeleteButtonContent;
