710991e82891361bd69b0681b56c2168c26bd864
KunhoLee <gunho1020@gmail.com>
2018-02-11 00:35:24 +0900

    ENH: init EditButtonContent Component

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import DeleteButton from './components/DeleteButton';
+import EditButtonContent from './components/EditButtonContent';
 
 export default class App extends React.Component {
   render() {
@@ -9,7 +9,7 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <View style={styles.test}>
-          <DeleteButton ownEvent={{ onPress: () => console.log('test hi') }} />
+          <EditButtonContent />
         </View>
       </View>
     );
---
+++ components/EditButtonContent/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+function EditButtonContent() {
+  return (
+    <Text>EditbuttonContent</Text>
+  );
+}
+
+export default EditButtonContent;
