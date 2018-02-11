f63625028f8bddc056b9e52216edb76c584c4338
KunhoLee <gunho1020@gmail.com>
2018-02-12 01:04:15 +0900

    ENH: NonEditingButtonGroup 컴포넌트 prop-types 체크

--- components/molecules/ActionButtons/NonEditingButtonGroup.js
+++ components/molecules/ActionButtons/NonEditingButtonGroup.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // import Components;
 import ActionButton from '../ActionButton';
@@ -12,5 +13,10 @@ function NonEditingButtonGroup({ startEditing, deleteItem }) {
     </Row>
   );
 }
+
+NonEditingButtonGroup.propTypes = {
+  startEditing: PropTypes.func.isRequired,
+  deleteItem: PropTypes.func.isRequired,
+};
 
 export default NonEditingButtonGroup;
