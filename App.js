7548b5d745fb93731d30f48afeb198d3c13d3bcd
KunhoLee <gunho1020@gmail.com>
2018-02-11 11:36:10 +0900

    ENH: makeActionButtons HOC 로 ActionButtons 생성

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import makeActionButtons from './hoc/makeActionButtons';
 import ActionButton from './components/ActionButton';
+
+const ActionButtons = makeActionButtons(
+  () => <ActionButton content="✅" ownEvent={{ onPress: () => console.log('test sleeppppyyppy finishEditing') }} />,
+  () => <ActionButton content="✏️" ownEvent={{ onPress: () => console.log('test sleeppppyyppy edit') }} />,
+  () => <ActionButton content="❌" ownEvent={{ onPress: () => console.log('test sleeppppyyppy delete') }} />,
+);
 
 export default class App extends React.Component {
   render() {
@@ -9,9 +16,12 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <View style={styles.test}>
-          <ActionButton content="✏️" ownEvent={{ onPress: () => console.log('test sleeppppyyppy edit') }} />
-          <ActionButton content="❌" ownEvent={{ onPress: () => console.log('test sleeppppyyppy delete') }} />
-          <ActionButton content="✅" ownEvent={{ onPress: () => console.log('test sleeppppyyppy finishEditing') }} />
+          <ActionButtons
+            isEditing={false}
+            finishEditing={() => console.log('finishEditing')}
+            startEditing={() => console.log('startEditing')}
+            deleteItem={() => console.log('deleteItem')}
+          />
         </View>
       </View>
     );
